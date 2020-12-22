9920238965c70507578777a48c6dd7ce72fe8a0b
Rajvardhanthakur <rajthakur3619@gmail.com>
2020-12-22 10:24:22 +0530

    initial commit

---
+++ screens/Search.js
@@ -0,0 +1,16 @@
+
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import Header from './Header';  
+
+const Search = () => {
+
+  return (
+    <View>
+      <Header name="Search Screen" />
+      <Text>Search Screen</Text>
+    </View>
+  );
+};
+
+export default Search;
